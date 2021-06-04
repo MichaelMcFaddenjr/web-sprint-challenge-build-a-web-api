6318bebd55239d6f7fbb3fb921215a0db58e9fcb
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-06-04 09:29:19 -0600

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,52 @@
-// Write your "projects" router here!
+const express = require('express');
+const { checkBody, checkProjectsId } = require('./projects-middleware');
+
+
+const router = express.Router();
+
+const Projects = require('./projects-model');
+
+router.get('/', (req, res, next) => {
+  Projects.get()
+    .then(projects => {
+      res.status(200).json(projects)
+    })
+    .catch(next)
+});
+
+router.get('/:id', checkProjectsId, (req, res, next) => {
+  res.json(req.newBody)
+})
+
+router.post('/', checkBody, (req, res, next) => {
+  Projects.insert(req.body)
+    .then(project => {
+      res.status(201).json(project)
+    })
+    .catch(next)
+})
+
+router.put('/:id', checkProjectsId, checkBody, (req, res, next) => {
+  Projects.update(req.params.id, req.body)
+    .then((project) => {
+      res.status(200).json(project)
+    })
+    .catch(next)
+})
+
+router.delete('/:id', checkProjectsId, (req, res, next) => {
+  Projects.remove(req.params.id)
+    .then(project => {
+      res.status(200).json(project)
+    })
+    .catch(next)
+})
+
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    message:'something went wrong, sorry',
+    err: err.message,
+    stack: err.stack,
+  })
+})
+module.exports = router;
